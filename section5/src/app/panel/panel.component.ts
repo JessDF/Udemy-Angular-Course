611ab9a42d764f2fa7536b62ca4e0c08326eebc0
jdowding <jdowding@ciginsurance.com>
2020-01-28 10:20:29 -0800

    ngContent and ngContainer

---
+++ section5/src/app/panel/panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bootstrap-panel',
+  template: './panel.component.html',
+  styleUrls: ['./panel.component.css']
+})
+export class PanelComponent {
+  constructor(){}
+}
