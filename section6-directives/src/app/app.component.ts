fcd109576081e847e48695866d752246ce75132c
JessDF <jdowding@csumb.edu>
2020-01-28 21:03:38 -0800

    Assignment 4: Like Component

--- section6-directives/src/app/app.component.ts
+++ section6-directives/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'section6-directives';
+  tweet = {
+    body: '...',
+    likesCount: 10,
+    isLiked: true
+  }
 }
