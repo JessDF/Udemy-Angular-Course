5b04d8dc9c262e9e56f56f8e8d4d40990848b714
JessDF <jdowding@csumb.edu>
2020-01-28 20:47:28 -0800

    Update a few items for panel component and add readme

--- section5/src/app/panel/panel.component.ts
+++ section5/src/app/panel/panel.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bootstrap-panel',
-  template: './panel.component.html',
+  templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css']
 })
 export class PanelComponent {
