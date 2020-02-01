96872c14c13fe7b31a588bac781f33dbd178a400
JessDF <jdowding@csumb.edu>
2020-02-01 12:27:36 -0800

    ngClass

--- section6-directives/src/app/ng-class/ng-class.component.ts
+++ section6-directives/src/app/ng-class/ng-class.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-class',
   templateUrl: './ng-class.component.html',
   styleUrls: ['./ng-class.component.css']
 })
-export class NgClassComponent implements OnInit {
+export class NgClassComponent {
+  isSelected: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  onClick() {
+    this.isSelected = !this.isSelected;
   }
 
 }
