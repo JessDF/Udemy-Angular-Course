33f2f1b84bcfe029e56f45db86f7a6ff64879bbc
JessDF <jdowding@csumb.edu>
2020-02-01 12:21:11 -0800

    Init ng-class component

--- section6-directives/src/app/app.module.ts
+++ section6-directives/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LikeComponent } from './like/like.component';
 import { NgSwitchCaseComponent } from './ng-switch-case/ng-switch-case.component';
 import { NgForComponent } from './ng-for/ng-for.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LikeComponent,
     NgSwitchCaseComponent,
-    NgForComponent
+    NgForComponent,
+    NgClassComponent
   ],
   imports: [
     BrowserModule
