9a3b203e9dc93fa0471110d3efcf5f96c0d910fb
JessDF <jdowding@csumb.edu>
2020-03-11 06:58:01 -0700

    Creating Controls Programatically

--- section8-reactive-forms/src/app/signup-form/signup-form.component.ts
+++ section8-reactive-forms/src/app/signup-form/signup-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-form',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup-form.component.css']
 })
 export class SignupFormComponent {
+  /* AbstractControl is the base class (parent) of FormControl and FormGroup
+   * FormGroup and FormControl inherit functionality from it
+   * The first argument of FormGroup is key-value pairs with value an instance of the class derived from AbstractControl class
+   */
+  form = new FormGroup({ //This is how we explicitly make FormGroup and FormControl objects
+    username : new FormControl(),
+    password : new FormControl()
+  });
 
 }
