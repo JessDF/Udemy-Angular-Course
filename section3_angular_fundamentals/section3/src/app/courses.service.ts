f71347da904a8d84c7a59aa56dca80d562d35de3
JessDF <jdowding@csumb.edu>
2020-04-10 20:08:40 -0700

    Reorganize section 3 files

--- section3_angular_fundamentals/section3/src/app/courses.service.ts
+++
@@ -1,7 +0,0 @@
-//Angular doesn't have a decorator for services
-export class CoursesService {
-    getCourses() {
-        //For now hard coding as we aren't ready to get in complexity of consuming HTTP service
-        return ["course1", "course2", "course3"];
-    }
-}
--- section3_angular_fundamentals/section3/src/app/email.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({ //decorator would only be needed if have dependencies within the constructor
-  providedIn: 'root'
-})
-export class EmailService {
-
-  constructor() { }
-}
